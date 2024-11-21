7b0788d8b6635566d9cf2ccd20e6085ed35359ba
jorgeslfreitas <jorgeslfreitas@gmail.com>
2024-11-21 07:21:49 -0300

    adjustments: ajuste do arquivo index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -5,7 +5,7 @@ import { router } from "./routes/index.js";
 const app = express();
 
 app.use(cors());
-app.use(express,json());
+app.use(express.json());
 app.use(router);
 
 app.listen(3002, () => {
